898a827f17c6e8e7042b8c3e8a83e344ad2c0326
ProboticsX <36927669+ProboticsX@users.noreply.github.com>
2018-03-01 00:27:50 +0530

    code for bluetooth module hc05 using arduino

---
+++ bluetooth.ino
@@ -0,0 +1,49 @@
+String voice="";
+
+int led1 = 7;
+int led2 = 3;
+int led3 = 4;
+
+void allon() {
+  digitalWrite (led1, HIGH);
+  digitalWrite (led2, HIGH);
+  digitalWrite (led3, HIGH);
+
+}
+
+void alloff() {
+  digitalWrite (led1, LOW);
+  digitalWrite (led2, LOW);
+  digitalWrite (led3, LOW);
+}
+
+void setup() {
+  Serial.begin(38400);
+  pinMode(led1, OUTPUT);
+  digitalWrite (led1, HIGH);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  while(Serial.available()) {
+    delay(10);
+    char c=Serial.read();
+    Serial.println(c);
+    if(c=='#')
+    {break; }
+    voice += c;
+}
+
+if (voice.length() > 0) {
+  Serial.println(voice);
+  if (voice == "*LED on"||voice == "*lights on" )
+  {allon() ; }
+  else if (voice == "*LED of" || voice == "*LED off" || voice == "*lights of" || voice == "*lights off")
+  {alloff() ; }
+  
+voice="";
+}
+}
+
+
