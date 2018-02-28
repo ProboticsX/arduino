209a3b0319160434f180f83edb4f52ca5a2bb35b
ProboticsX <36927669+ProboticsX@users.noreply.github.com>
2018-03-01 01:00:06 +0530

    code for sound sensor using arduino

---
+++ sound.ino
@@ -0,0 +1,29 @@
+int sensor=2;
+int led=3;
+
+void setup() {
+//Serial.begin(9600);
+
+pinMode(sensor,INPUT);
+pinMode(led,OUTPUT);  
+// put your setup code here, to run once:
+
+}
+
+void loop() {
+int pin=digitalRead(sensor);
+
+if(pin==1)
+digitalWrite(led,HIGH);
+else
+digitalWrite(led,LOW);
+
+//delay(1000);
+//Serial.print("Sound in Hz:");
+//Serial.println(pin);
+
+//delay(1000);
+  
+  // put your main code here, to run repeatedly:
+
+}
