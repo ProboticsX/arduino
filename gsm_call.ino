0175d41999047265662cadd3fe60e95016ec19e0
ProboticsX <36927669+ProboticsX@users.noreply.github.com>
2018-03-01 00:36:37 +0530

    code for gsm module using arduino (calling)

---
+++ gsm_call.ino
@@ -0,0 +1,25 @@
+#include<SoftwareSerial.h>
+SoftwareSerial gsm(3,4);
+//tx:9 rx:10
+
+
+void setup() {
+  Serial.begin(9600);
+  gsm.begin(9600);
+ // delay(1000);
+  
+  gsm.println("ATD+919910077157;"); //AT command to call from GSM
+  delay(20000);
+  gsm.println("ATH");
+Serial.print("hi");  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  
+   //if (gsm.available())
+   //Serial.write(gsm.read());
+  // put your main code here, to run repeatedly:
+  
+}
