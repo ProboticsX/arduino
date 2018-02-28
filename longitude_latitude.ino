0a42357acac497781bdcc55d5fcc3829c3353cba
ProboticsX <36927669+ProboticsX@users.noreply.github.com>
2018-03-01 00:32:44 +0530

    code for using the gps module with arduino uno

---
+++ longitude_latitude.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+
+TinyGPS gps;
+SoftwareSerial sgps(5, 6); //rx-6, tx-5
+     
+void setup()
+{
+  Serial.begin(9600);
+  sgps.begin(9600);
+}
+
+void loop()
+{
+  
+  
+  while (sgps.available())
+  {
+    
+    int c = sgps.read();
+    if (gps.encode(c))
+    {
+float slat,slon;
+      gps.f_get_position(&slat, &slon);
+      Serial.print("Latitude :");
+      Serial.println(slat, 6);
+      Serial.print("Longitude:");
+      Serial.println(slon, 6);
+    }
+  }
+}
